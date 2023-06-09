303edcabad657758fce320e26d8cf168884bf5f4
Miseon <soup7912@gmail.com>
2023-06-09 17:18:34 +0900

    test

--- src/store.js
+++ src/store.js
@@ -8,9 +8,9 @@ export default createStore({
           "name": "디자이너인 당신을 위해",
           "comment": "디자인 환경에 적합한 클라우드 노트북 서비스",
           "spec": [
-            "GPU 대박 1",
-            "스토리지 짱큼 1",
-            "예쁜 Mac OS 1"
+            "빠른 그래픽 처리 가능 GPU",
+            "넉넉한 메모리 사양",
+            "우수한 색 재현률 Mac OS"
           ]
         },
         {
@@ -18,19 +18,19 @@ export default createStore({
           "name": "AI 개발 환경이 필요하신가요?",
           "comment": "AI 개발 환경을 위한 클라우드 노트북 서비스",
           "spec": [
-            "GPU...",
+            "Multi-GPU 가능",
             "추가 스토리지 제공",
-            "Linux"
+            "개발에 적합한 Linux"
           ]
         },
         {
           "src": '3Ddesigner_plan_img',
-          "name": "3D 그래픽 디자인",
+          "name": "그래픽 소프트웨어 사용자를 위해",
           "comment": "높은 그래픽 사양의 클라우드 노트북 서비스",
           "spec": [
-            "3D 그래픽 소프트웨어를 위한 고성능 GPU 제공",
-            "스토리지 대박임",
-            "예쁜 Mac OS 3"
+            "3D 작업을 위한 고성능 GPU 제공",
+            "무거운 파일 커다란 스토리지",
+            "3D 디스플레이에 적합한 Mac OS"
           ]
         }
       ]
